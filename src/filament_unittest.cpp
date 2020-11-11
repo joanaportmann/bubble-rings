269082ee3686547be5ccb7d1dde207bb5bb2feec
joana <joana.portmann@students.unibe.ch>
2020-11-11 09:47:58 +0100

    WIB: Sample unit test with google test

---
+++ src/filament_unittest.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+namespace {
+    TEST(UpdateFilamentTest, Zero)
+    {
+           EXPECT_EQ(1, 1);
+    }
+
+    TEST(ShouldBeFalse, 1)
+    {
+        EXPECT_EQ(1, 2);
+    }
+}
